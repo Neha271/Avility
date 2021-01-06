92b433b1f51eb5eb413987f7274e6114e7c39985
Neha271 <47948584+Neha271@users.noreply.github.com>
2021-01-06 11:56:31 +0530

    Initial commit

---
+++ project/app/App.tsx
@@ -0,0 +1,33 @@
+import React, { useMemo } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import Spaces from '@availity/spaces';
+import PageHeader from '@availity/page-header';
+import qs from 'query-string';
+import { Request, Response } from './areas';
+import { Footer } from './shared';
+
+const App = (): JSX.Element => {
+  const { search } = useLocation();
+
+  const spaceId = useMemo(() => qs.parse(search).spaceId, [search]);
+
+  return (
+    <Spaces spaceIds={[spaceId]} clientId="test">
+      <Container id="app-container">
+        <PageHeader spaceId={spaceId} appName="Appeal Request Form" />
+        <Switch>
+          <Route exact path="/">
+            <Request />
+          </Route>
+          <Route path="/response">
+            <Response />
+          </Route>
+        </Switch>
+        <Footer />
+      </Container>
+    </Spaces>
+  );
+};
+
+export default App;
